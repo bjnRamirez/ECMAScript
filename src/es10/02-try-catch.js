1d9c850097550d4069de99d34061b068d5ccbe2c
Benjamin Ramirez <bjnramirezbjn@gmail.com>
2022-12-04 10:41:24 -0500

    ES10

---
+++ src/es10/02-try-catch.js
@@ -0,0 +1,12 @@
+/* Las siguientes características de ES10 o ES2019 que aprenderás son: parámetro opcional de catch y un método para tranformar arrays a objetos */
+try{
+    hello();
+}catch(error){
+    console.log(error);
+}
+
+try{
+    anotherFunction();
+}catch{
+    console.log('Esto es un error')
+}
---
+++ src/es10/03-fromentries.js
@@ -0,0 +1,23 @@
+/* Cómo transformar un array de arrays en un objeto
+El método Object.fromEntries devuelve un objeto a partir de un array donde sus elementos son las entries en forma [propiedad, valor].
+ */
+const entries = new Map([["name", "benjamin"], ["age", 24]]);
+console.log(entries)
+console.log(Object.fromEntries(entries));
+
+
+const arrayEntries = [
+    [ 'name', 'Andres' ],
+    [ 'email', 'andres@correo.com' ],
+    [ 'age', 23 ]
+]
+
+const usuario = Object.fromEntries(arrayEntries) 
+
+console.log(usuario)
+/* {
+    name: 'Andres',
+    email: 'andres@correo.com',
+    age: 23
+    }
+  */
